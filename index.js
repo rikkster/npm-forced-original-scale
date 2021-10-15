530e1987dff1f78d6ed889a1f8a63f3d8c17db27
Rikky <rikkster@yandex.ru>
2021-10-16 01:31:40 +0300

    Unfollow ES

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-function originalScale( appContainerId ) {
+module.exports.fromUrl = function ( appContainerId ) {
 
   var App = document.getElementById( appContainerId );
   App.style.zoom = 1 / devicePixelRatio;
   App.style['-moz-transform'] = `scale(${1 / devicePixelRatio})`;
 
 }
-
-export default originalScale;
