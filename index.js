943ccfb894b8a72f996b388ff1e9f9e71ab65469
Rikky <rikkster@yandex.ru>
2021-10-17 12:54:51 +0300

    scale in firefox does not works correclty and deprecated

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function originalScale( appContainerId ) {
 
   var App = document.getElementById( appContainerId );
   App.style.zoom = 1 / devicePixelRatio;
-  App.style['-moz-transform'] = `scale(${1 / devicePixelRatio})`;
 
 }
 
