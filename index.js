a948b605ab38d4e61f83f17e10676b8611f7ff63
Rikky <rikkster@yandex.ru>
2021-10-17 13:57:53 +0300

    Ignore movile devices, and refresh scale with resizing window

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function originalScale( appContainerId ) {
 
   var App = document.getElementById( appContainerId );
-  App.style.zoom = 1 / devicePixelRatio;
+  
+  if ( window.innerWidth >= 1225 ) { 
+
+    App.style.zoom = 1 / devicePixelRatio;
+
+  } else {
+
+    App.style.zoom = 1;
+
+  }
+
+  function refreshScale() { console.log(`refreshScale`); originalScale( appContainerId ) }
+
+  var resizeTimeout;
+  
+  window.onresize = function() {
+
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(refreshScale, 100);
+
+  };
 
 }
 
